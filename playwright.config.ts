08907159d720090db7554b0c811cc7d24bf90c2c
nagarajukios1985-cyber <nagarajukios1985@gmail.com>
2026-05-29 01:13:46 +0530

    config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,14 +7,9 @@ export default defineConfig({
 
   use: {
     headless: true,
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
   },
 
   reporter: [
-    ['list'],
-
-    // 👇 ONLY YOUR CUSTOM DASHBOARD
     ['./reporter/ArtifactHtmlReporter', {
       outputFile: 'artifacts/test-report.md',
       outputHtml: 'artifacts/test-report.html',
